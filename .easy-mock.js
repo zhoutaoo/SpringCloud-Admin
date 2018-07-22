13c43c003bc7297f18b6b4d7d1ded1faf335e0b8
周涛 <zhoutaoo@foxmail.com>
2018-07-22 20:07:58 +0800

    增加easy-mock的配置文件

---
+++ .easy-mock.js
@@ -0,0 +1,15 @@
+// .easymock.js
+module.exports = {
+  output: "api",
+  template: "axios",
+  projects: [
+    {
+      id: "58fef6ac5e43ae5dbea5eb52",
+      name: "group" // 生成到 api/group 目录下。
+    },
+    {
+      id: "58fef6ac5e43ae5dbea5eb51",
+      name: "top" // 生成到 api/top 目录下。
+    }
+  ]
+};
